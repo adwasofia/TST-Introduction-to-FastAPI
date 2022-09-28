f29cc5775fb8ac0da0cf69715f965227740baff1
adwasofia <93809575+adwasofia@users.noreply.github.com>
2022-09-28 07:30:11 +0700

    add json file and try read from json file

--- IntroductiontoFastAPI_18220109/addNIMnama.py
+++ IntroductiontoFastAPI_18220109/addNIMnama.py
@@ -1,24 +1,34 @@
+# NIM: 18220109
+# Nama: Adwa Sofia
+
+import json
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-NIMnamaMahasiswa = {
-    0: {
-        "NIM":0,
-        "Nama":"Kosong"
-    }
-}
+
+with open("NIMnamaMahasiswa.json", "r") as read_file:
+    data = json.load(read_file)
+app = FastAPI()
+#@app.get('/menu/{item_id}')
+#async def read_menu(item_id: int):
+    #for menu_item in data['menu']:
+        #if menu_item['id'] == item_id:
+            #return menu_item
+    #raise HTTPException(
+        #status_code=404, detail=f'Item not found'
+#)
 
 class NIMnama(BaseModel): # the data model
     NIM: int
     Nama: str
-app = FastAPI()
+#app = FastAPI()
 @app.post("/NIMNama")
 async def add_NIM_nama(new_NIM_nama: NIMnama):
-    if new_NIM_nama.NIM in NIMnamaMahasiswa:
+    if new_NIM_nama.NIM in data:
         return {"NIM tersebut telah tersimpan."}
     else:
-        NIMnamaMahasiswa[new_NIM_nama.NIM] = {
+        data[new_NIM_nama.NIM] = {
             "NIM": new_NIM_nama.NIM,
             "Nama": new_NIM_nama.Nama
             }
-        return NIMnamaMahasiswa
+        return data
