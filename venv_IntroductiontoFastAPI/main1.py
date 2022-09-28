96bb6dfe5ac6bc363f255a7eaea808c7ba670863
adwasofia <18220109@std.stei.itb.ac.id>
2022-09-28 09:33:52 +0700

    delete unnecessary comments

--- venv_IntroductiontoFastAPI/main1.py
+++ venv_IntroductiontoFastAPI/main1.py
@@ -9,24 +9,12 @@ from pydantic import BaseModel
 with open("NIMnamaMahasiswa.json", "r") as read_file:
     data = json.load(read_file)
 app = FastAPI()
-#@app.get('/menu/{item_id}')
-#async def read_menu(item_id: int):
-    #for menu_item in data['menu']:
-        #if menu_item['id'] == item_id:
-            #return menu_item
-    #raise HTTPException(
-        #status_code=404, detail=f'Item not found'
-#)
 
-#jsonString = json.dumps(aList)
-#jsonFile = open("data.json", "w")
-#jsonFile.write(jsonString)
-#jsonFile.close()
 
 class NIMnama(BaseModel): # the data model
     NIM: int
     Nama: str
-#app = FastAPI()
+
 @app.post("/NIMNamas")
 async def add_NIM_nama(new_NIM_nama: NIMnama):
     if new_NIM_nama.NIM in data:
@@ -39,8 +27,4 @@ async def add_NIM_nama(new_NIM_nama: NIMnama):
             }
             ]
         data["NIMnamaMahasiswa"].append(new_data)
-        #new_jsonString = jsonString = json.dumps(new_data)
-        #jsonWrite = open("NIMnamaMahasiswa.json", "w")
-        #jsonWrite.write(new_jsonString)
-        #jsonWrite.close()
         return data
