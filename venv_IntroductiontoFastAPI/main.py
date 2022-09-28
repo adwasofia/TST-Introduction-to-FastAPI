30c9096f9649df578bee4d3a774f66a16f26aba2
adwasofia <18220109@std.stei.itb.ac.id>
2022-09-28 09:37:39 +0700

    rename main1 to main, add comments

--- venv_IntroductiontoFastAPI/main.py
+++ venv_IntroductiontoFastAPI/main.py
@@ -1,5 +1,6 @@
 # NIM: 18220109
 # Nama: Adwa Sofia
+# Program ini menggunakan file NIMnamaMahasiswa.json yang terdapat pada folder yang sama dengan program ini.
 
 import json
 from fastapi import FastAPI
@@ -9,38 +10,22 @@ from pydantic import BaseModel
 with open("NIMnamaMahasiswa.json", "r") as read_file:
     data = json.load(read_file)
 app = FastAPI()
-#@app.get('/menu/{item_id}')
-#async def read_menu(item_id: int):
-    #for menu_item in data['menu']:
-        #if menu_item['id'] == item_id:
-            #return menu_item
-    #raise HTTPException(
-        #status_code=404, detail=f'Item not found'
-#)
 
-#jsonString = json.dumps(aList)
-#jsonFile = open("data.json", "w")
-#jsonFile.write(jsonString)
-#jsonFile.close()
 
 class NIMnama(BaseModel): # the data model
     NIM: int
     Nama: str
-#app = FastAPI()
+
 @app.post("/NIMNamas")
 async def add_NIM_nama(new_NIM_nama: NIMnama):
     if new_NIM_nama.NIM in data:
         return {"NIM tersebut telah tersimpan."}
     else:
-        new_data = {
+        new_data = [
+            {
             "NIM": new_NIM_nama.NIM,
             "Nama": new_NIM_nama.Nama
             }
+            ]
         data["NIMnamaMahasiswa"].append(new_data)
-        #read_file.seek(0)
-        #json.dump(data, read_file, indent = 4)
-        #new_jsonString = json.dumps(new_data)
-        #jsonWrite = open("NIMnamaMahasiswa.json", "w")
-        #jsonWrite.write(new_jsonString)
-        #jsonWrite.close()
         return data
